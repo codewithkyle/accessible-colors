58dc891db7030d87f5e25708e283aaba87aa2f71
Kyle Andrews <codingwithkyle@gmail.com>
2021-06-12 09:45:53 -0400

    Updated service worker caching logic

---
+++ asset-manager.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    src: [
+        {
+            files: "./public/assets/*.js",
+            publicDir: "/assets"
+        },
+    ],
+    output: "./public/service-worker-assets.js",
+};
